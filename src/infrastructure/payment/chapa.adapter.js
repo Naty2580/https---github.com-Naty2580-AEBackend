912db1a2886e17316df96dde0a069b5997e2547f
natnaelnegash <natnaelnegash95@gmail.com>
2026-04-07 14:15:22 +0300

    Updated the LedgerEntry model to have transferRef added payment initialzer, fund releaser, webhook verifier in chapaAdapter.js

--- src/infrastructure/payment/chapa.adapter.js
+++ src/infrastructure/payment/chapa.adapter.js
@@ -1,11 +1,56 @@
 import crypto from 'node:crypto';
+import axios from 'axios';
 import config from '../../config/env.config.js';
 
-export const verifyChapaSignature = (signature, payload) => {
-  const hash = crypto
-    .createHmac('sha256', config.CHAPA_WEBHOOK_SECRET)
-    .update(JSON.stringify(payload))
-    .digest('hex');
-  
-  return hash === signature;
-};
+
+const chapaApi = axios.create({
+  baseURL: 'https://api.chapa.co/v1',
+  headers: {
+    Authorization: `Bearer ${config.CHAPA_SECRET_KEY}`,
+    'Content-Type': 'application/json'
+  }
+})
+
+
+export const chapaAdapter = {
+
+  async initializeTransaction(data) {
+    try {
+      const response = await chapaApi.post('/transaction/initialize', data)
+      return response.data;
+    } catch (error) {
+      console.error('Payment initialization Error: ', error.response?.data || error.message)
+      throw new Error('Failed to initialize payment with chapa')
+    }
+  },
+
+  async verifyTransaction(transactionId) {
+    try {
+      const response = await chapaApi.get(`/transaction/${transactionId}`)
+      return response.data;
+    } catch (error) {
+      console.error('Payment verification Error: ', error.response?.data || error.message)
+      throw new Error('Failed to verify payment with chapa')
+    }
+  },
+
+  async transferFunds(data) {
+    try {
+      const response = await chapaApi.post('/transfers', data)
+      return response.data;
+    } catch (error) {
+      console.error('Transfer Error: ', error.response?.data || error.message)
+      throw new Error('Failed to transfer funds with chapa')
+    }
+  },
+
+  verifyWebhookSignature(signature, payload) {
+    const hash = crypto
+      .createHmac('sha256', config.CHAPA_WEBHOOK_SECRET)
+      .update(JSON.stringify(payload))
+      .digest('hex');
+    
+    return hash === signature;
+  }
+}
+
